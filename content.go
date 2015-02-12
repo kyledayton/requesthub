b413b9c07122200d783821b570a2133784fb5177
Kyle Dayton <thedarkdownloader@gmail.com>
2015-02-12 14:50:44 -0600

    Added clear link

--- content.go
+++ content.go
@@ -7,16 +7,29 @@ PAGE_CONTENT = `
 	<title>RequestHub</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>
+
+function fetchRequests() {
+	jQuery.get("/requests", function(data) {
+		jQuery("#requests").html(JSON.stringify(data));
+	});
+}
+
 jQuery(document).ready(function() {
-jQuery.get("/requests", function(data) {
-        jQuery("#requests").html(JSON.stringify(data));
-});
+	jQuery("#clear").click(function() {
+		jQuery.get("/clear", function(d) {
+			fetchRequests();
+		});
+	});
+
+	fetchRequests();
 });
 </script>
 
 </head>
 <body>
 <h1>RequestHub</h1>
+<a id="clear" href="#">Clear</a>
+
 <div id="requests"></div>
 </body>
 </html>
