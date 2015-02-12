7d9406e9af9d9eef808e5888e12d8618256ac77f
Kyle Dayton <thedarkdownloader@gmail.com>
2015-02-12 14:30:40 -0600

    Basic page content

--- content.go
+++ content.go
@@ -5,9 +5,19 @@ PAGE_CONTENT = `
 <html>
 <head>
 	<title>RequestHub</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+jQuery(document).ready(function() {
+jQuery.get("/requests", function(data) {
+        jQuery("#requests").html(JSON.stringify(data));
+});
+});
+</script>
+
 </head>
 <body>
 <h1>RequestHub</h1>
+<div id="requests"></div>
 </body>
 </html>
 `
