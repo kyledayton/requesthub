92ea645dc13f8451d793b50ff6becfe8f8bfe7f2
Jesse Campos <jessec@spiceworks.com>
2015-02-12 15:18:17 -0600

    separating response data for ui

--- content.go
+++ content.go
@@ -7,11 +7,23 @@ PAGE_CONTENT = `
 	<title>RequestHub</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>
-jQuery(document).ready(function() {
-jQuery.get("/requests", function(data) {
-        jQuery("#requests").html(JSON.stringify(data));
-});
-});
+(function($) {
+  $(document).ready(function() {
+  $.get("/requests", function(data) {
+    var requests = [];
+    Object.keys(data.requests).map(function(request) {
+      requests.push('<div class="request"><h1> request ' + request + '</h1>' +
+        '<div class="headers"><h2>headers</h2><pre><code>' +
+        JSON.stringify(data.requests[request].headers, null, 4) +
+        '</pre></code></div>' +
+        '<div class="body"><h2>body</h2><pre><code>' +
+        JSON.stringify(data.requests[request].body, null, 4) +
+        '</pre></code></div></div>');
+    });
+    $("#requests").html(requests.join(''));
+  });
+  });
+})(jQuery);
 </script>
 
 </head>
